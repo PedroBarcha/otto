4e8c755cf1069648e3bbd87ccc1e8c8f9962e141
esskidri <esskidri@gmail.com>
2018-06-05 18:00:21 +0200

    automatic voice detection

--- lib/record.py
+++ lib/record.py
@@ -1,35 +1,109 @@
 import pyaudio
 import wave
- 
-FORMAT=pyaudio.paInt8
-CHANNELS=1
-RATE=44100*2
-CHUNK=1024
-RECORD_SECONDS=4
-WAVE_OUTPUT_FILENAME="records/user.wav"
+import time
+import audioop
+import numpy
+import thread
+import threading
 
-def record(): 
-	audio=pyaudio.PyAudio()
-	 
-	# start Recording
-	stream=audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True,
-	                frames_per_buffer=CHUNK)
-	print("recording...")
-	frames=[]
-	 
-	for i in range(0, int(RATE/CHUNK*RECORD_SECONDS)):
-		data=stream.read(CHUNK)
-		frames.append(data)
-	print("finished recording")
+
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100*2
+CHUNK = 1024
+RECORD_SECONDS = 5
+WAVE_OUTPUT_FILENAME = "file.wav"
+flag2=0
+
+threadLock = threading.Lock()
+threads = []
  
-	# stop Recording
+def set_trs(stream):
+	max_noise = 0
+	volumes = []
+	t=time.time()
+	while((time.time()-t<5)):
+		current_volume=audioop.rms(stream.read(CHUNK), 2)
+		volumes.append(current_volume)
+		print("noise : " + str(current_volume))
+	max_noise = max(volumes)
+	print ("max noise value : " + str(max_noise))
+	return max_noise
+
+class myThread(threading.Thread):
+	def __init__(self):
+		threading.Thread.__init__(self)
+	def run(self):
+ 		record()
+
+def record():
+	frames = []
+	audio = pyaudio.PyAudio()
+
+	# start Recording
+	stream = audio.open(format=FORMAT, channels=CHANNELS,
+	                rate=RATE, input=True,
+	                frames_per_buffer=CHUNK)
+
+	while(flag2==0):
+		for i in range(0, int(RATE/CHUNK)):
+			data = stream.read(CHUNK)
+			frames.append(data)
 	stream.stop_stream()
 	stream.close()
 	audio.terminate()
- 
-	waveFile=wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+
+	waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
 	waveFile.setnchannels(CHANNELS)
 	waveFile.setsampwidth(audio.get_sample_size(FORMAT))
 	waveFile.setframerate(RATE)
 	waveFile.writeframes(b''.join(frames))
 	waveFile.close()
+
+
+
+def detectVoice():
+	audio1 = pyaudio.PyAudio()
+	# start Recording
+	stream1 = audio1.open(format=FORMAT, channels=CHANNELS,
+		                rate=RATE, input=True,
+		                frames_per_buffer=CHUNK)
+
+	 
+
+
+	THRESHOLD=set_trs(stream1)
+	THRESHOLD=1.4*THRESHOLD
+	flag=0
+
+	while (1):
+		if( audioop.rms(stream1.read(CHUNK),2) > THRESHOLD ):
+			myThread = myThread()
+			myThread.start()
+			print "START RECORDING"
+			break
+
+
+	while(flag == 0):
+		if( audioop.rms(stream1.read(CHUNK),2) > THRESHOLD ): 
+			print "RECORDING.."
+		else:
+			t1=time.time()
+			while (audioop.rms(stream1.read(CHUNK),2) <= THRESHOLD ):
+				print "RECORDING SILENCE.."
+				if (time.time()-t1 > 1 ):
+					print "STOP"
+					flag=1
+					flag2=1
+					break
+	for t in threads:
+		t.join()
+	print "Exiting Main Thread"
+				
+
+
+
+
+
+
+
